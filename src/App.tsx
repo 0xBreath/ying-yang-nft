b80ee98476a34da2da11675de7dc214b0f7b0909
bowtiedbreath <mindmoneymusic108@gmail.com>
2021-11-21 12:38:44 -0700

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ import {
 import { WalletDialogProvider } from "@solana/wallet-adapter-material-ui";
 import { createTheme, ThemeProvider } from "@material-ui/core";
 
+
 const treasury = new anchor.web3.PublicKey(
   process.env.REACT_APP_TREASURY_ADDRESS!
 );
@@ -43,30 +44,57 @@ const startDateSeed = parseInt(process.env.REACT_APP_CANDY_START_DATE!, 10);
 
 const txTimeout = 30000; // milliseconds (confirm this works for your project)
 
+
 const theme = createTheme({
-    palette: {
-        type: 'dark',
+  palette: {
+    
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#F5F5F5',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
     },
-    overrides: {
-        MuiButtonBase: {
-            root: {
-                justifyContent: 'flex-start',
-            },
-        },
-        MuiButton: {
-            root: {
-                textTransform: undefined,
-                padding: '12px 16px',
-            },
-            startIcon: {
-                marginRight: 8,
-            },
-            endIcon: {
-                marginLeft: 8,
-            },
-        },
+    secondary: {
+      light: '#0066ff',
+      main: '#0044ff',
+      // dark: will be calculated from palette.secondary.main,
+      contrastText: '#ffcc00',
     },
+    // Used by `getContrastText()` to maximize the contrast between
+    // the background and the text.
+    contrastThreshold: 3,
+    // Used by the functions below to shift a color's luminance by approximately
+    // two indexes within its tonal palette.
+    // E.g., shift from Red 500 to Red 300 or Red 700.
+    tonalOffset: 0.2,
+  },
+  overrides: {
+      // MuiButtonBase: {
+      //     root: {
+      //         justifyContent: 'center',
+      //     },
+      // },
+      MuiListItem:{
+        root:{
+          background: '#d9d9d9',
+                padding: 0,
+        }
+      },
+      MuiButton: {
+          root: {
+              textTransform: undefined,
+              padding: '12px 16px',
+          },
+          startIcon: {
+              marginRight: 8,
+          },
+          endIcon: {
+              marginLeft: 8,
+          },
+      },
+  },
 });
+
 
 const App = () => {
   const endpoint = useMemo(() => clusterApiUrl(network), []);
@@ -84,20 +112,20 @@ const App = () => {
 
   return (
       <ThemeProvider theme={theme}>
-        <ConnectionProvider endpoint={endpoint}>
-          <WalletProvider wallets={wallets} autoConnect={true}>
-            <WalletDialogProvider>
-              <Home
-                candyMachineId={candyMachineId}
-                config={config}
-                connection={connection}
-                startDate={startDateSeed}
-                treasury={treasury}
-                txTimeout={txTimeout}
-              />
-            </WalletDialogProvider>
-          </WalletProvider>
-        </ConnectionProvider>
+          <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect={true}>
+              <WalletDialogProvider>
+                <Home
+                  candyMachineId={candyMachineId}
+                  config={config}
+                  connection={connection}
+                  startDate={startDateSeed}
+                  treasury={treasury}
+                  txTimeout={txTimeout}
+                />
+              </WalletDialogProvider>
+            </WalletProvider>
+          </ConnectionProvider>
       </ThemeProvider>
   );
 };
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import Countdown from "react-countdown";
-import { Button, CircularProgress, Snackbar } from "@material-ui/core";
+import { Button, CircularProgress, Snackbar, makeStyles, Typography } from "@material-ui/core";
 import Alert from "@material-ui/lab/Alert";
 
 import * as anchor from "@project-serum/anchor";
@@ -18,14 +18,15 @@ import {
   mintOneToken,
   shortenAddress,
 } from "./candy-machine";
+import { fontFamily } from "@mui/system";
 
 const ConnectButton = styled(WalletDialogButton)``;
 
-const CounterText = styled.span``; // add your styles here
+const CounterText = styled.span``; 
 
-const MintContainer = styled.div``; // add your styles here
+const MintContainer = styled.div``;
 
-const MintButton = styled(Button)``; // add your styles here
+const MintButton = styled(Button)``; 
 
 export interface HomeProps {
   candyMachineId: anchor.web3.PublicKey;
@@ -164,9 +165,11 @@ const Home = (props: HomeProps) => {
     props.candyMachineId,
     props.connection,
   ]);
+  
+  const classes = useStyles()
 
   return (
-    <main>
+    <main className={classes.cover}>
       {wallet && (
         <p>Wallet {shortenAddress(wallet.publicKey.toBase58() || "")}</p>
       )}
@@ -179,15 +182,38 @@ const Home = (props: HomeProps) => {
 
       {wallet && <p>Remaining: {itemsRemaining}</p>}
 
+      <div className={classes.mintArea}>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+
+        <div className={classes.words}>
+          <h1>YIN YANG NFT SERVICE</h1>
+          <h2>Membership Pass</h2>
+          <br></br>
+          <h3>Custom premium services</h3>
+          <h3>Top DAO's alpha from insider network</h3>
+          <h3>Guaranteed whitelists spots for upcoming hyped drops</h3>
+          <br></br>
+        </div>
       <MintContainer>
         {!wallet ? (
-          <ConnectButton>Connect Wallet</ConnectButton>
+          <ConnectButton className={classes.centerConnect}>Connect Wallet</ConnectButton>
         ) : (
+          <>
+          <Typography className = {classes.price}>      
+            Price: 0.33 SOL
+          </Typography>
+
           <MintButton
+            className={classes.mint}
             disabled={isSoldOut || isMinting || !isActive}
             onClick={onMint}
             variant="contained"
           >
+            
             {isSoldOut ? (
               "SOLD OUT"
             ) : isActive ? (
@@ -205,8 +231,10 @@ const Home = (props: HomeProps) => {
               />
             )}
           </MintButton>
+          </>
         )}
       </MintContainer>
+      </div>
 
       <Snackbar
         open={alertState.open}
@@ -237,5 +265,125 @@ const renderCounter = ({ days, hours, minutes, seconds, completed }: any) => {
     </CounterText>
   );
 };
+
+const useStyles = makeStyles(theme => ({
+  words: {
+    color: '#000',
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  mint: {
+    background: '#FFF',
+    color: 'black',
+    border: 3,
+    borderRadius: 3,
+    height: 70,
+    width: 150,
+    fontSize: 22,
+    font: 'Helvetica',
+    padding: '30px 30px',
+
+    '&:disabled':{
+      backgroundColor:'#11C8C5',
+      color:'white',
+      opacity:'0.5'
+    },
+    '&:hover': {
+      backgroundColor: '#EC683E',
+      boxShadow: '0 0 50px white',
+      color: 'black'
+    }
+  },
+  bar: {
+    position: 'absolute',
+    top: '80%'
+  },
+  soldOut: {
+    color: 'whitesmoke'
+  },
+  cover: {
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    margin: '0 auto',
+    background: '#FFFFF3',
+    fontFamily: 'copperplate'
+  },
+  progressBarClass: {
+    transform:'translate(0%, 150%)',
+    height:'10px',
+    width:'500px',
+    borderRadius:'50px',
+    backgroundColor:'black'
+  },
+  centerConnect: {
+    background: '#000',
+    color: 'white',
+    border: 0,
+    borderRadius: 3,
+    height: 100,
+    width: 250,
+    fontSize: 22,
+    font: 'Monaco',
+    padding: '30px 30px',
+
+    '&:disabled':{
+      backgroundColor:'#11C8C5',
+      color:'white',
+      opacity:'0.5'
+    },
+    '&:hover': {
+      backgroundColor: '#76D4AA',
+      boxShadow: '0 0 50px white',
+      color: 'black'
+    }
+  },
+  mintArea: {
+    color: 'white',
+    position: 'absolute', left: '50%', top: '20%',
+    transform: 'translate(-50%, -50%)',
+    fontSize: '60px',
+    textAlign: 'center',
+  },
+  price: {
+    fontSize: '25px',
+    marginTop:'20px',
+    fontFamily:'Courier New',
+    justifyContent: "center",
+    color: '#000',
+    fontWeight: 'bold'
+  },
+  price1:{
+    fontSize: '50px',
+    color:'white',
+    // display: "flex",
+    position:'absolute',
+    margin:'0 auto',
+    top:'50%',
+    left:'50%',
+    transform:'translate(-50%,-400%)',
+    fontFamily:'Lato',
+    alignSelf:'center',
+  
+  },
+  howManyRemaining:{
+    fontSize:'25px',
+    fontFamily:'Lato'
+  },
+  homeButton: {
+    borderRadius: 3,
+    background: 'linear-gradient(to right, #E7E9BB, #403B4A)',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 45,
+    width: 150,
+    padding: '5px 5px',
+    position: 'absolute', left: '10%', top: '10%',
+    transform: 'translate(-50%, -50%)',
+  }
+}));
 
 export default Home;
